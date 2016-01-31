efbd20ec2f0dd750d5624d8cfb16a2641722f967
Richard Huang <flyerhzm@gmail.com>
2016-02-01 07:39:01 +0800

    multi-inserts after tuning

--- multi-inserts/app/controllers/posts_controller.rb
+++ multi-inserts/app/controllers/posts_controller.rb
@@ -2,8 +2,10 @@ class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def import
+    posts = []
     10.times do
-      Post.create title: Faker::Lorem.sentence, body: Faker::Hipster.paragraph, user_id: rand(100) + 1
+      posts << Post.new(title: Faker::Lorem.sentence, body: Faker::Hipster.paragraph, user_id: rand(100) + 1)
     end
+    Post.import posts
   end
 end
